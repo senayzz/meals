6b5021bf7495ea571ff4993a2139fc4fa3c19892
Sena Yılmaz <ylmz_sena@hotmail.com>
2025-08-22 16:41:26 +0300

    Adding Explicit animation for gridview.

--- lib/screens/categories_view.dart
+++ lib/screens/categories_view.dart
@@ -7,18 +7,47 @@ import 'package:meals/screens/meals_view.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 //Bu bir screen olduğu için screen olarak adlandırıyoruz.
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key, required this.avaliableMeals});
 
   // final void Function(Meal meal) onToggleFavorite;
   final List<Meal> avaliableMeals;
+
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+
   //Seçilen kategoriyi meals sayfasında göstermek için yazacağımız bir fonksiyon
-  //ilk defa stateless widgete fonksiyon ekledik
-  //Statefullda context globally idi ancak statelessda değil tanımlalamamız lazım fonksiyona girdi olarak vermemiz lazım.
   void _selectCategory(BuildContext context, Category category) {
     //Seçilen kategoriye (category.id) ait olan tüm yemekleri filteredMeals listesine ekle.
     final filteredMeals =
-        avaliableMeals
+        widget.avaliableMeals
             .where((meal) => meal.categories.contains(category.id))
             .toList();
     //  Navigator.push(context, route); ikiside aynı eyi yapıyor.
@@ -32,29 +61,47 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      padding: EdgeInsets.all(24),
-      //Bir satırda ne kadar element listeleneceğini söyledik.
-      //horizontally 2 column
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
-        //colonlar arası boşluk horizontally
-        crossAxisSpacing: 20,
-        //vertically boşluk
-        mainAxisSpacing: 20,
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: GridView(
+        padding: EdgeInsets.all(24),
+        //Bir satırda ne kadar element listeleneceğini söyledik.
+        //horizontally 2 column
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          //colonlar arası boşluk horizontally
+          crossAxisSpacing: 20,
+          //vertically boşluk
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          // avaliableCateries.map((category) => CategoryGridItem(category: category)).toList
+          //for loopun alternatifi
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            ),
+        ],
       ),
-      children: [
-        // avaliableCateries.map((category) => CategoryGridItem(category: category)).toList
-        //for loopun alternatifi
-        for (final category in availableCategories)
-          CategoryGridItem(
-            category: category,
-            onSelectCategory: () {
-              _selectCategory(context, category);
-            },
+      builder:
+          (context, child) => SlideTransition(
+            position: _animationController.drive(
+              Tween(
+                begin: const Offset(0, 0.3),
+                end: const Offset(0, 0),
+              ).animate(
+                CurvedAnimation(
+                  parent: _animationController,
+                  curve: Curves.easeInOut,
+                ),
+              ),
+            ),
+            child: child,
           ),
-      ],
     );
   }
 }
